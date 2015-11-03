5ddce8813f593ad3bec78f56b7f7167d86c5797a
Alucia <analu1478@gmail.com>
2015-11-03 17:14:45 -0500

    add

--- js/chat.js
+++ js/chat.js
@@ -9,18 +9,20 @@
 // });
 $(document).ready(function(){
 
-
-	 
+	$("#go").click(function(e){
+			e.preventDefault();  //previene el evento click , q se detenga
+			var texto = $("#chat").val();					
+			var miPrimerMensaje = $("<p></p>").text(texto,color); //etiquetas al vuelo		
+			var color = $('#info').click(function(e){
+				e.preventDefault();
+				miPrimerMensaje.css("background-color","blue");		
+			});			
+			$(".resultado").append(miPrimerMensaje);
+	});
 
 	$('#info').click(function(e){
 		miPrimerMensaje.css("background-color","red");
-		$("#go").click(function(e){
-			e.preventDefault();  //previene el evento click , q se detenga
-			var texto = $("#chat").val();
-			var miPrimerMensaje = $("<p></p>").text(texto); //etiquetas al vuelo
-			$(".resultado").append(miPrimerMensaje);
-
-	});
+		
 	});
 	$('#war').click(function(e){
 		miPrimerMensaje.css("background-color","yellow");
