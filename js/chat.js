89a15f049f953e0f0deae492074769d101911111
Alucia <analu1478@gmail.com>
2015-11-03 16:26:49 -0500

    archivo base

---
+++ js/chat.js
@@ -0,0 +1,28 @@
+// $(document).ready(function(){
+// 	$("#go").click(function(e){
+// 			e.preventDefault();  //previene el evento click , q se detenga
+// 			var texto = $("#chat").val();
+// 			var miPrimerMensaje = $("<p></p>").text(texto); //etiquetas al vuelo
+// 			$(".resultado").append(miPrimerMensaje);
+// 	}); 
+
+// });
+$(document).ready(function(){
+
+
+	 
+
+	$('#info').click(function(e){
+		miPrimerMensaje.css("background-color","red");
+		$("#go").click(function(e){
+			e.preventDefault();  //previene el evento click , q se detenga
+			var texto = $("#chat").val();
+			var miPrimerMensaje = $("<p></p>").text(texto); //etiquetas al vuelo
+			$(".resultado").append(miPrimerMensaje);
+
+	});
+	});
+	$('#war').click(function(e){
+		miPrimerMensaje.css("background-color","yellow");
+	});
+});
