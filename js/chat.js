a4e9cffe7494bbe06d950bbe0318a233b16ffc1a
Alucia <analu1478@gmail.com>
2015-11-03 17:24:23 -0500

    add color

--- js/chat.js
+++ js/chat.js
@@ -12,8 +12,7 @@ $(document).ready(function(){
 	$("#go").click(function(e){
 			e.preventDefault();  //previene el evento click , q se detenga
 			var texto = $("#chat").val();					
-			var miPrimerMensaje = $("<p></p>").text(texto); //etiquetas al vuelo		
-						
+			var miPrimerMensaje = $("<p></p>").text(texto); //etiquetas al vuelo					
 			miPrimerMensaje.addClass("alert-info");
 			$(".resultado").append(miPrimerMensaje);
 	});
