9771150e4055c4c7670299e9abdb61468401a83a
Alucia <analu1478@gmail.com>
2015-11-04 09:34:09 -0500

    agregando select

--- js/chat.js
+++ js/chat.js
@@ -12,30 +12,13 @@ $(document).ready(function(){
 			e.preventDefault();  //previene el evento click , q se detenga
 			var texto = $("#chat").val();					
 			var miPrimerMensaje = $("<p></p>").text(texto); //etiquetas al vuelo					
-			miPrimerMensaje.addClass("alert-info");
 			$(".resultado").append(miPrimerMensaje);
 	});
 
 	$("#go").click(function(e){
-			e.preventDefault();  //previene el evento click , q se detenga
-			var texto = $("#chat").val();					
-			var miPrimerMensaje = $("<p></p>").text(texto); //etiquetas al vuelo					
-			miPrimerMensaje.addClass("alert-warning");
-			$(".resultado").append(miPrimerMensaje);
-	});
-	$("#go").click(function(e){
-			e.preventDefault();  //previene el evento click , q se detenga
-			var texto = $("#chat").val();					
-			var miPrimerMensaje = $("<p></p>").text(texto); //etiquetas al vuelo					
-			miPrimerMensaje.addClass("alert-success");
-			$(".resultado").append(miPrimerMensaje);
-	});
-	$("#go").click(function(e){
-			e.preventDefault();  //previene el evento click , q se detenga
-			var texto = $("#chat").val();					
-			var miPrimerMensaje = $("<p></p>").text(texto); //etiquetas al vuelo					
-			miPrimerMensaje.addClass("alert-danger");
-			$(".resultado").append(miPrimerMensaje);
+			e.preventDefault();
+			var color = $("#color option:selected").val();				
+			$(".resultado p:last-child").addClass(color);
 	});
 
 });
